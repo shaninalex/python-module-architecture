bb4da8f9eea81d7a661d6e03ae3caa85e9684664
shaninalex <shanin.x@gmail.com>
2026-09-06 13:41:39 +0300

    chore: product card and images

--- src/modules/catalog/domain/product.py
+++ src/modules/catalog/domain/product.py
@@ -1,5 +1,12 @@
 import datetime
-from dataclasses import dataclass
+import random
+from dataclasses import dataclass, field
+
+
+def random_product_image() -> str:
+    """Make url for random shoes image from src/adapters/web/static/images """
+    return f"images/sneakers-{random.randint(1, 9)}.webp"
+
 
 @dataclass
 class ProductModel:
@@ -9,3 +16,7 @@ class ProductModel:
     short_description: str
     created_at: datetime.datetime
     updated_at: datetime.datetime
+
+    # NOTE:
+    # until I add images in schema - this will be image field
+    image: str = field(default_factory=random_product_image)
